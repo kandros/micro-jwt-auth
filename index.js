3362318c16cc9977c500c25ab449907a5245e0da
jaga <kandros5591@gmail.com>
2017-02-15 14:15:45 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const jwt = require('jsonwebtoken')
+
+module.exports = exports = (secret) => (fn) => {
+    return (req, res) => {
+        
+        if (!secret) {
+            throw Error('secret is undefined')
+        }
+        
+        const bearerToken = req.headers.authorization
+        
+        if (!bearerToken) {
+            res.writeHead(401)
+            res.end('missing Authorization header')
+            return
+        }
+        
+        try {
+            const token = bearerToken.split(' ')[1]
+            jwt.verify(token, secret);
+        } catch(err) {
+            res.writeHead(401)
+            res.end('invalid token in Authorization header')
+            return
+        }
+        
+        return fn(req, res)
+    }
+}
