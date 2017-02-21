7fec0d48f7e7ec24feafb8ae69279dc6d315926a
Stefano <stefano.armenes@gmail.com>
2017-02-21 19:28:29 +0100

    improved test coverage

--- test/index.test.js
+++ test/index.test.js
@@ -11,8 +11,26 @@ test('error throwed if secret undefined', () => {
 test('case of request has not authorization header', () => {
 
   const request = {
-    headers: {
+    headers: {}
+  }
 
+  const response = {
+    writeHead: jest.fn().mockImplementation(),
+    end: jest.fn().mockImplementation()
+  };
+
+  const result = jwtAuth('mySecret')()(request, response)
+
+  expect(result).toBeUndefined()
+  expect(response.writeHead).toHaveBeenCalledWith(401)
+  expect(response.end).toHaveBeenCalledWith('missing Authorization header')
+});
+
+test('that all works fine: no errors', () => {
+
+  const request = {
+    headers: {
+      authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IldhbHRlciBXaGl0ZSIsImFkbWluIjp0cnVlfQ.YyF_yOQsTSQghvM08WBp7VhsHRv-4Ir4eMQvsEycY1A'
     }
   }
 
@@ -21,9 +39,31 @@ test('case of request has not authorization header', () => {
     end: jest.fn().mockImplementation()
   };
 
-  const result = jwtAuth('mySecret')()(request, response);
+  const result = jwtAuth('mySecret')(() => 'Good job!')(request, response)
+
+  expect(result).toEqual('Good job!')
+  expect(response.writeHead).toHaveBeenCalledTimes(0)
+  expect(response.end).toHaveBeenCalledTimes(0)
+
+})
+
+test('wrong bearer case', () => {
+
+  const request = {
+    headers: {
+      authorization: 'Bearer wrong.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IldhbHRlciBXaGl0ZSIsImFkbWluIjp0cnVlfQ.YyF_yOQsTSQghvM08WBp7VhsHRv-4Ir4eMQvsEycY1A'
+    }
+  }
+
+  const response = {
+    writeHead: jest.fn().mockImplementation(),
+    end: jest.fn().mockImplementation()
+  };
+
+  const result = jwtAuth('mySecret')(() => {})(request, response)
 
   expect(result).toBeUndefined()
-  expect(response.writeHead).toHaveBeenCalledWith(401);
-  expect(response.end).toHaveBeenCalledWith('missing Authorization header');
-});
+  expect(response.writeHead).toHaveBeenCalledWith(401)
+  expect(response.end).toHaveBeenCalledWith('invalid token in Authorization header')
+
+})
